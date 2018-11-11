0366a69e8a77fa6a6166069180729d2db23db7c0
Dimitris Zer <dimitris-007@hotmail.com>
2018-11-11 17:00:33 +0200

    test

--- src/components/Main/initialize.js
+++ src/components/Main/initialize.js
@@ -1,3 +1,5 @@
-export const instanceLocator = process.env.INSTANCE_LOCATOR;
+export const instanceLocator = "v1:us1:70765878-2ac8-4e66-938d-b18c7503aded";
 
 export const tokenProvider = "https://evening-harbor-95971.herokuapp.com/auth";
+
+console.log(instanceLocator, tokenProvider);
